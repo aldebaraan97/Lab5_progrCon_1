62d76e1706dc30d4043110ee0099b9625fd11503
Aldebaraan <aldecanedo97@gmail.com>
2024-11-01 15:17:51 -0400

    .

--- src/ConversionProgram.java
+++ src/ConversionProgram.java
@@ -1,19 +1,56 @@
+import java.util.Scanner;
+
 public class ConversionProgram {
     public static void main(String[] args) {
-
+        double userInput;
         // TODO: Welcome text
+        System.out.println("This program converts distance in meters to Kilometers, inches and feet.");
 
         // TODO: Prompt user for input
+        System.out.println("Enter a distance in meters:");
+        userInput = userInput();
 
         // TODO: Display menu
+        menu();
+        userInput = userInput();
+
+        switch (userInput) {
+            case 1 -> showKilometers(userInput);
+            case 2 -> showInches(userInput);
+            case 3 -> showFeet(userInput);
+            case 4 -> quit(userInput);
+            default -> System.out.println("Invalid option.");
+        };
 
     }
 
-    // TODO: toKM(double meters)
+    public static double userInput() {
+        Scanner input = new Scanner(System.in);
+        double distance;
+        distance = input.nextDouble();
+        return distance;
+    }
 
-    // TODO: toInches(double meters)
+    public static void showKilometers(double meters) {
+        System.out.println(meters * 0.001);
+    }
 
-    // TODO: toFeet(double meters)
+    public static void showInches(double meters) {
+        System.out.println(meters * 39.37);
+    }
 
-    // TODO: quit()
+    public static void showFeet(double meters) {
+        System.out.println(meters * 3.281);
+    }
+
+    // TODO: menu()
+    public static void menu() {
+        System.out.println("Type the number of the option to execute function:");
+        System.out.println("1. Convert to kilometers." +
+                "\n2. Convert to inches." +
+                "\n3. Convert to feet." +
+                "\n4. Quit.");
+    }
+
+    // TODO: quit(int quit)
 }
