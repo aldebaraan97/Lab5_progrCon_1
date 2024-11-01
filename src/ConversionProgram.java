0f571ebf38109cc0757fb754e12c601d9056ef50
Aldebaraan <aldecanedo97@gmail.com>
2024-11-01 14:57:08 -0400

    Prototyping of ConversionProgram class

---
+++ src/ConversionProgram.java
@@ -0,0 +1,19 @@
+public class ConversionProgram {
+    public static void main(String[] args) {
+
+        // TODO: Welcome text
+
+        // TODO: Prompt user for input
+
+        // TODO: Display menu
+
+    }
+
+    // TODO: toKM(double meters)
+
+    // TODO: toInches(double meters)
+
+    // TODO: toFeet(double meters)
+
+    // TODO: quit()
+}
